8cfce935709252f0a00da8b0b64809034d2a1b25
vsavkin <avix1000@gmail.com>
2017-03-31 11:25:33 -0400

    i18n: add i18n support

--- build/aot.js
+++ build/aot.js
@@ -3,17 +3,25 @@ const {CommonsChunkPlugin} = require('webpack').optimize;
 const {AotPlugin} = require('@ngtools/webpack');
 const nodeModules = path.join(process.cwd(), 'node_modules');
 
-module.exports = function () {
+module.exports = function (env) {
+  const locale = env.locale;
+
+  const i18nConfig = (!locale || locale === 'en') ? {} : {
+    i18nFile: './translations/messages.' + locale + '.xlf',
+    i18nFormat: 'xlf',
+    locale: locale
+  };
+
   return {
     plugins: [
-      new AotPlugin({
+      new AotPlugin(Object.assign({
         "mainPath": "main.ts",
         "hostReplacementPaths": {
           "environments/environment.ts": "environments/environment.ts"
         },
         "exclude": [],
         "tsConfigPath": "src/tsconfig.app.json"
-      }),
+      }, i18nConfig)),
       new CommonsChunkPlugin({
         "name": "inline",
         "minChunks": null
